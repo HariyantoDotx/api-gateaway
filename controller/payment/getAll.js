9f6fe930b6249db286fdcb4e4caec54be6b43f58
hariyanto97 <hariyantolikeipa@gmail.com>
2024-08-03 21:27:02 +0800

    update admin

--- controller/payment/getAll.js
+++ controller/payment/getAll.js
@@ -1,21 +1,23 @@
 const apiAdapter = require("../apiAdapter");
 const catchError = require("../catchError");
 
-const {URL_SERVICE_PAYMENT} = process.env;
+const { URL_SERVICE_PAYMENT } = process.env;
 
 const foodApi = apiAdapter(URL_SERVICE_PAYMENT);
 
+module.exports = async (req, res) => {
+  try {
+    const { status } = req.query;
+    const params = { status };
 
-module.exports = async(req, res) => {
-    try {   
-        const userId = req.user.data.id
-        const isAdmin = req.user.data.rules = "admin"
-        const params = {user_id : userId}
-        const food = await foodApi.get(`/api/order`,{
-            params : isAdmin? {} : params
-        })
-        return res.json(food.data)
-    } catch (error) {
-        catchError(error. res)
-    }
-}
+    const userId = req.user.data.id;
+    const isUser = (req.user.data.rules === "user");
+    if (isUser) params.user_id = userId;
+    const food = await foodApi.get(`/api/order`, {
+      params,
+    });
+    return res.json(food.data);
+  } catch (error) {
+    catchError(error.res);
+  }
+};
